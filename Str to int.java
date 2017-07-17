d35ea5ab883564fe414980950e6d92f0837e981c
keerthanajanani <keerthijanani66@gmail.com>
2017-07-17 10:46:09 +0530

    Create Str to int.java

---
+++ Str to int.java
@@ -0,0 +1,14 @@
+# Str-to-int
+import java.io.*;
+import java.util.*;
+public class Str to int
+{
+public static void main(String args[])throws IOException
+{
+int a;
+BufferedReader br=new BufferedReader(new InputStreamReader(br.readLine());
+String str1=br.readLine();
+a=Integer.parseInt(Str1);
+System.out.println(a);
+}
+}
